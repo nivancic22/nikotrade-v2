dd8ed8cd268e72fb6adba19a03412f599795c6e7
Niko Ivančić <nivancic22@foi.unizg.hr>
2026-02-03 12:45:35 +0100

    Katalog poboljsan

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from .models import Proizvod  
 
 def index(request):
     return render(request, 'shop/index.html')
 
 def katalog(request):
-    #dohvatanje proizvoda iz baze
-    return render(request, 'shop/katalog.html')
+    kategorija_slug = request.GET.get('kategorija')
+    
+    if kategorija_slug:
+        proizvodi_iz_baze = Proizvod.objects.filter(kategorija=kategorija_slug)
+    else:
+        proizvodi_iz_baze = Proizvod.objects.all()
+    
+    return render(request, 'shop/katalog.html', {'proizvodi': proizvodi_iz_baze})
 
 def kontakt(request):
     return render(request, 'shop/kontakt.html')
