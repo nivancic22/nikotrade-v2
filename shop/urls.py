f3c72847b05a3a65c3e69e277f111a39c4295d75
Niko Ivančić <nivancic22@foi.unizg.hr>
2026-02-03 03:57:59 +0100

    Dodan grid sa predloscima proizvoda i update za navbar

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('katalog/', views.katalog, name='katalog'), 
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'shop/index.html')
+
+def katalog(request):
+    #dohvatanje proizvoda iz baze
+    return render(request, 'shop/katalog.html')
